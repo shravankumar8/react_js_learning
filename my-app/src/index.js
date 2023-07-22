7e6fc23bb50db5609b7d212d1da83c517ac6bf12
shravankumar8 <kumashravan5@gmail.com>
2023-07-22 15:09:19 +0530

    getting hands on react

---
+++ my-app/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals'
+import Main from './main'
+import './test.css'
+
+const root =ReactDOM.createRoot(document.getElementById('root')) 
+root.render(
+  <React.StrictMode>
+    <App />
+    <Main />
+  </React.StrictMode>
+)
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+/*import React from 'react';
+import reactDom from 'react-d0m/client'; 
+import './index.css';
+import App from './App'
+const root =reactDom.createRoot(document.getElementById("root"))
+root.render(
+  <React.strictMode>
+<app />
+  </React.strictMode>
+)
+*/
+
+  
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ my-app/src/main.js
@@ -0,0 +1,5 @@
+import React from "react";
+function display_name(){
+    return <p>react is best </p>
+}
+export default display_name
